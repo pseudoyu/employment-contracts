41e0630fcd615f0570f5d5133eda538d0e3ad521
pseudoyu <pseudoyu@connect.hku.hk>
2023-09-03 11:19:51 +0800

    test: add forge test init config

--- contracts/mocks/OpenBuildToken.sol
+++ contracts/mocks/OpenBuildToken.sol
@@ -1,11 +1,12 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
+
 pragma solidity 0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract OpenBuildToken is ERC20 {
-    constructor() ERC20("OPENBUILD", "OBT") {
-        _mint(msg.sender, 10 * 10 ** 8 * 10 ** 18);
+    constructor() ERC20("OpenBuildToken", "OBT") {
+        _mint(msg.sender, 10000000000 * 10 ** 6);
     }
 
     function decimals() public view virtual override returns (uint8) {
